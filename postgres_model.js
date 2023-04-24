e2bb09ddad5da6065b7a1b0f1ebde0e30f7448bb
sdi1400258 <81802463+sdi1400258@users.noreply.github.com>
2023-04-24 13:45:05 +0200

    Update postgres_model.js

--- postgres_model.js
+++ postgres_model.js
@@ -1,7 +1,7 @@
 
 var pg_port=1212;
 var pg_host="gitguardians.com:9082/BLUDB";
-var pg_user="root";
-var pg_pass="sup3rstr0ngpass1ForGG";
+var pg_user="DevOps2023";
+var pg_pass="DD2482_Aut0mated_S0ftw4r3_T3st1ng!";
 
 var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
